890e401dfbc6e6b2726989adf6fef82bcd755c01
hoangquan1503 <hoangquan.20060315.808@gmail.com>
2025-06-25 15:08:17 +0700

    final

--- factorial.py
+++ factorial.py
@@ -1,3 +1,36 @@
+import os
+import streamlit as st
+
+def load_users():
+    try:
+        if os.path.exists("users.txt"):
+            with open(r"D:\AIO2025\users.txt", "r", encoding = "utf-8") as f:
+                users = [line.strip() for line in f.readlines() if line.strip()]
+            return users
+        else:
+            st.error("Users file not found.")
+            return []
+    except Exception as e:
+        st.error(f"An error occurred while reading file users: {e}")
+        return []
+def login_page():
+    st.title("Login Page")
+    username = st.text_input("Username: ")
+    if username:
+        if st.button("Login"):
+            users = load_users()
+            if username in users:
+                st.session_state.logged_in = True
+                st.session_state.username = username
+                st.rerun()
+            else :
+                st.session_state.show_greeting  = True
+                st.session_state.username = username
+                st.rerun()
+    else :
+        st.warning("Please enter a username.")
+        
+    
 def factorial(n):
     if n < 0:
         return "No factorial for negative numbers"
@@ -7,12 +40,41 @@ def factorial(n):
     for i in range(1, n):
         result *= i
     return result
-def main():
-    import streamlit as st
+def factorial_page():
+    
     st.title("Factorial Calculator")
+    st.write("Hi, " + st.session_state.username + "! Welcome to the Factorial Calculator.")
+    if st.button("Logout"):
+        st.session_state.logged_in = False
+        st.session_state.username = ""
+        st.rerun()
+    st.divider()
     number = st.number_input("Enter a number", min_value = 0, max_value = 900)
     if st.button("Calculate Factorial"):
         result = factorial(number)
         st.write(f"The factorial of {number} is: {result}")
+def greeting_page():
+    st.title("Hi, " + st.session_state.username )
+    st.write("You do not have permission to access the calculator")
+    if st.button("Logout"):
+        st.session_state.logged_in = False
+        st.session_state.show_greeting = False
+        st.session_state.username = ""
+        st.rerun()
+def main():
+    if "logged_in" not in st.session_state:
+        st.session_state.logged_in = False
+    if "username" not in st.session_state:
+        st.session_state.username = ""
+    if "show_greeting" not in st.session_state:
+        st.session_state.show_greeting = False
+
+    if st.session_state.logged_in:
+        factorial_page()
+    elif st.session_state.show_greeting:
+        greeting_page()
+    else:
+        login_page()
 if __name__ == "__main__":
     main()
+    
